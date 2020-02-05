9b20d01fbf4f309853fcd421c39f056b65123d35
brandonxiang <1542453460@qq.com>
2020-02-05 21:15:14 +0800

    build: format

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,43 @@
 <script context="module">
   import axios from 'axios';
 
-  export async function preload () {
-		 const { data } = await axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=20`);
-		 return { photos: data };
-	 }
+  export async function preload() {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos?_limit=20`
+    );
+    return { photos: data };
+  }
 </script>
 
 <script>
-	export let photos = [];
+  export let photos = [];
 </script>
-
-<style>
-	.photos {
-		width: 100%;
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-gap: 8px;
-	}
-
-	figure, img {
-		width: 100%;
-		margin: 0;
-	}
-</style>
 
 <h1>Photo album</h1>
 
 <div class="photos">
-	{#each photos as photo}
-		<figure>
-			<img src={photo.thumbnailUrl} alt={photo.title}>
-			<figcaption>{photo.title}</figcaption>
-		</figure>
-	{:else}
-		<!-- this block renders when photos.length === 0 -->
-		<p>loading...</p>
-	{/each}
+  {#each photos as photo}
+    <figure>
+      <img src="{photo.thumbnailUrl}" alt="{photo.title}" />
+      <figcaption>{photo.title}</figcaption>
+    </figure>
+  {:else}
+    <!-- this block renders when photos.length === 0 -->
+    <p>loading...</p>
+  {/each}
 </div>
+
+<style>
+  .photos {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 8px;
+  }
+
+  figure,
+  img {
+    width: 100%;
+    margin: 0;
+  }
+</style>
